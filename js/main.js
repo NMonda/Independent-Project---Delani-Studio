45a9a7d2ad7f94bbc08b54b5484a2c9a1f1830be
NMONDA <ndegemonda@gmail.com>
2022-11-09 06:50:06 +0300

    main.js toggling effect

--- js/main.js
+++ js/main.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
       $(this).hide();
       $("#design_pic").toggle().show();
     });
+    $("#development_pic").click(function () {
+        $(this).hide();
+        $("#development").toggle().show();
+      });
+      $("#development").click(function () {
+        $(this).hide();
+        $("#development_pic").toggle().show();
+      });
     
