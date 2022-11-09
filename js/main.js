7fa9a5a3cad20fd2f5f81621dcdce499ac01b00c
NMONDA <ndegemonda@gmail.com>
2022-11-09 06:51:23 +0300

    website responsiveness

--- js/main.js
+++ js/main.js
@@ -15,4 +15,29 @@ $(document).ready(function () {
         $(this).hide();
         $("#development_pic").toggle().show();
       });
-    
+      $("#product_pic").click(function () {
+        $(this).hide();
+        $("#product").toggle().show();
+      });
+      $("#product").click(function () {
+        $(this).hide();
+        $("#product_pic").toggle().show();
+      });
+      $("#work1").hover(
+        function () {
+          $("#project1").show().css({
+            background: "rgba(0, 0, 0, 0.6)",
+            height: "73%",
+            width: "75%",
+            "font-size": "121%",
+            color: "white",
+          });
+          $(this).css({ cursor: "pointer" });
+          if ($(window).width() < 850) {
+            $("#project1").css({
+              "font-size": "71%",
+              "border-width": "2.5px",
+              width: "60%",
+            });
+          }
+        },
